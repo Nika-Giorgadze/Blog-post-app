65eca6bd871d84648bf563a3ec9cf4b5ebfabab5
irakli giorgadze <irakligiorgadze15@gmail.com>
2024-11-19 16:46:08 +0400

    adding dtos

---
+++ src/main/java/com/example/BlogpostApp/dto/PostDTO.java
@@ -0,0 +1,15 @@
+package com.example.BlogpostApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDTO {
+    private String title;
+    private String content;
+}
